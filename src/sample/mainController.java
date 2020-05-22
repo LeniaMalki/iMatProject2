5c75c37214cc501185e41cedde893cfcf1e5ea8f
EpicMunz <mans.josefsson99@gmail.com>
2020-05-22 10:48:32 +0200

    mainController

--- src/sample/mainController.java
+++ src/sample/mainController.java
@@ -3,7 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
-public class SampleController {
+public class mainController {
     public Label helloWorld;
 
     public void sayHelloWorld(ActionEvent actionEvent) {
--- src/sample/searchController.java
+++ src/sample/searchController.java
@@ -1,4 +1,63 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+
+import javax.swing.text.html.ImageView;
+
 public class searchController {
+
+    //Cards?
+
+
+    //Framework
+    @FXML private ImageView Logo;
+    @FXML private Pane dealsCategory;
+    @FXML private TextField searchBar;
+    @FXML private Button searchButton;
+    @FXML private Button helpButton;
+    @FXML private Button historyButton;
+    @FXML private Button personalButton;
+
+    //Cart
+    @FXML private ScrollPane scrollCart;
+    @FXML private Label totalPriceLabel;
+    @FXML private Button checkoutButton;
+
+    //Categories
+    @FXML private Pane Category;
+
+    //Kontaktuppgifter
+    @FXML private TextField fornamnTextField;
+    @FXML private TextField efternamnTextField;
+    @FXML private TextField telefonnummerTextField;
+    @FXML private Button vidareKontaktButton;
+    @FXML private Button tillbakaKontaktButton;
+
+    //Leveransuppgifter
+    @FXML private TextField adressTextField;
+    @FXML private TextField postnummerTextField;
+    @FXML private TextField stadTextField;
+    @FXML private RadioButton leveransdag1RadioButton;
+    @FXML private RadioButton leveransdag2RadioButton;
+    @FXML private Button vidareLeveransButton;
+    @FXML private Button tillbakaLeveransButton;
+
+    //Kontouppgifter
+    @FXML private TextField kortnummerTextField;
+    @FXML private TextField utgangsdatumTextField;
+    @FXML private TextField sakerhetskodTextField;
+    @FXML private Button vidareKontoButton;
+    @FXML private Button tillbakaKontoButton;
+
+    //Summering av order
+    @FXML private Label totalbeloppLabel;
+    @FXML private Label leveranstidLabel;
+    @FXML private Button slutforKopButton;
+    @FXML private Button tillbakaSummButton;
+
+    //Köp bekräftat
+    @FXML private Button tillbakaTillStartButton;
+
 }
